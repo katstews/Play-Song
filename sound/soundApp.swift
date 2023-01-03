c221b5e086b86b6ff57fb5db317e55c7c8af78bf
kashew <katherinec1488@gmail.com>
2023-01-03 14:02:52 -0800

    Initial Commit

---
+++ sound/soundApp.swift
@@ -0,0 +1,17 @@
+//
+//  soundApp.swift
+//  sound
+//
+//  Created by Katherine Chen on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct soundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
